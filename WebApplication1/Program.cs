b77a5441d5bc47b3b50e13307d58b5db59f37ea5
_ext Solod, Artem <artem.solod.ext@dieboldnixdorf.com>
2019-07-18 22:56:07 +0300

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHost = CreateWebHostBuilder(args).Build();
+            //webHost.RunAsService();
+            webHost.Run();
+
+
+            //Start AppContext.BaseDirectory
+
+
+//            Thread.Sleep(2000);
+        }
+
+
+
+        //static CancellationToken _cancellationToken = new CancellationToken();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using Winton.Extensions.Configuration.Consul;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        private IApplicationLifetime _appLifetime;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+
+            Configuration = new ConfigurationBuilder().AddConfiguration(configuration)
+                .AddConsul("site1/appsettings.json", default, options =>
+          {
+              options.ConsulConfigurationOptions =
+                                   cco => { cco.Address = new Uri("http://localhost:8500"); };
+              options.Optional = true;
+              options.ReloadOnChange = true;
+              options.OnLoadException = exceptioncontext => { exceptioncontext.Ignore = false; };
+
+          }).Build();
+
+            ChangeToken.OnChange(Configuration.GetReloadToken, OnChanged);
+
+            //var builder = new ConfigurationBuilder()
+            //    .SetBasePath(env.ContentRootPath)
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+            //builder.AddEnvironmentVariables("SECURE_");
+            //Configuration = builder.Build();
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IConfiguration>(Configuration);
+            //var q =  Configuration["SECURE_APP"] as string;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            _appLifetime = appLifetime;
+            appLifetime.ApplicationStopped.Register(OnStopped);
+            appLifetime.ApplicationStarted.Register(OnStarted);
+        }
+
+        bool _started;
+
+        private void OnStarted()
+        {
+            _started = true;
+        }
+
+        private void OnChanged()
+        {
+            Console.WriteLine("HELLO");
+            if (_started)
+            {
+                Debugger.Launch();
+                _appLifetime?.StopApplication();
+            }
+        }
+
+        private void OnStopped()
+        {
+            new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    UseShellExecute = true,
+                    FileName = @"C:\Program Files\dotnet\dotnet.exe",
+                    Arguments = @"exec C:\Users\VinT\source\repos\WebApplication1\WebApplication1\bin\Debug\netcoreapp2.1\WebApplication1.dll"
+                }
+            }.Start();
+
+
+            //new Process()
+            //{
+            //    StartInfo = new ProcessStartInfo()
+            //    {
+            //        UseShellExecute = true,
+            //        FileName = @"C:\Windows\System32\sc.exe",
+            //        Arguments = @"start atest"
+            //    }
+            //}.Start();
+
+        }
+    }
+}
